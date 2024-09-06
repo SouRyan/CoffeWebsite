8401fc72a640a304ab06a95bef947e0f4cfa81e3
Ryan Brayan <ryanbrayan.fs@gmail.com>
2024-09-06 13:04:29 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var splide = new Splide('.splide', {
+    perPage: 4,
+    rewind: true,
+});
+
+splide.mount();
+
+function updatePerPage() {
+    if (window.innerWidth <= 768) { // Para telas de celular
+        splide.options = { perPage: 2 };
+    } else if (window.innerWidth > 768 && window.innerWidth <= 968) { // Para telas intermediárias, como tablets
+        splide.options = { perPage: 2 };
+    } else if (window.innerWidth > 968 && window.innerWidth <= 1400) { // Para telas maiores, como laptops
+        splide.options = { perPage: 3 };
+    } else { // Para telas maiores que 1400px, como desktops
+        splide.options = { perPage: 4 };
+    }
+}
+
+// Chamar a função ao carregar a página e quando a janela for redimensionada
+window.addEventListener('resize', updatePerPage);
+updatePerPage();
